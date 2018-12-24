6d3201b7c19c71eb6ccfe75acb3aad3fa3ded4d9
BrendanSChang <BrendanSChang@gmail.com>
2018-12-24 10:20:50 -0500

    Updating with GET requests plus some styling

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # nyc-parking-lookup
 
 This is a basic application to look up parking data for businesses and business owners in NYC. The data is queried through the Yelp API and the NYC Legally Operating Businesses database.
+
+## Known Issues
+
+- The Yelp Fusion API business details endpoint does not seem to contain the desired parking data
+- The Legally Owned Businesses database is not case insensitive right now
--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
+const http = require("http");
+const https = require("https");
+const qs = require("querystring");
+const fs = require("fs");
+// This is for the NYC Legally Operating Businesses API.
+const options = {headers: {"X-App-Token": "YOUR_APP_TOKEN"}};
+const lobApi = "https://data.cityofnewyork.us/resource/p2mh-mrfv.json";
+// const yelpApi = "https://api.yelp.com/v3/businesses/";
 
-http.createServer(function(req, res) {
+http.createServer((req, res) => {
   if (req.method === "GET") {
-    fs.readFile("index.html", function(err, data) {
+    fs.readFile("index.html", (err, data) => {
       if (err) {
+        // TODO: This is probably the wrong error to send back.
         res.writeHead(404, {"Content-Type": "text/html"});
         return res.end("404 Not Found");
       }
@@ -15,8 +21,87 @@ http.createServer(function(req, res) {
       res.end();
     });
   } else if (req.method === "POST") {
-    res.writeHead(200, {"Content-Type": "text/html"});
-    res.end("not implemented yet");
+    let query = "";
+    req.on("data", (data) => {
+      query += data;
+    });
+    req.on("end", () => {
+      // Add on assumed values. The query string itself can be used directly
+      // for the Yelp API.
+      query += "&city=New York&state=NY&country=US";
+      const params = qs.parse(query);
+
+      // TODO: This is all very brittle. Some records are already being filtered
+      // out because of the casing of "NY" and "NEW YORK".
+      let lobArgs = ["license_status=Active", "address_state=NY", "address_city=NEW YORK"];
+      lobArgs.push("business_name=" + params.name);
+      const firstSpace = params.address1.indexOf(' ')
+      lobArgs.push("address_building=" + params.address1.slice(0, firstSpace));
+      lobArgs.push("address_street_name=" + params.address1.slice(firstSpace + 1));
+      https.get(lobApi + '?' + lobArgs.join('&'), options, (resp) => {
+        let match = "";
+        resp.on("data", (data) => {
+          match += data;
+        });
+        resp.on("end", () => {
+          // Guaranteed to be a list.
+          match = JSON.parse(match);
+          let type = "Leased";
+          // Industry has been observed to be "Garage", "Parking Lot", and
+          // "Garage and Parking Lot".
+          for (let m of match) {
+            // TODO: Is there some way to move this into the query?
+            if (m.industry &&
+                (m.industry.indexOf("Parking") != -1 || m.industry.indexOf("Garage") != -1)) {
+              type = "Owned";
+              break;
+            }
+          }
+
+          res.writeHead(200, {"Content-Type": "text/html"});
+          res.end(type);
+        });
+        resp.on("error", (err) => {
+          console.log("Error matching business: " + err.message);
+        });
+      });
+
+      // TODO: Unfortunately, the data here is not useful and it doesn't seem
+      // to be available through a different endpoint.
+      /*
+      https.get(yelpApi + "matches?" + query, options, (resp) => {
+        let match = "";
+        resp.on("data", (data) => {
+          match += data;
+        });
+        resp.on("end", () => {
+          match = JSON.parse(match);
+          if (match.businesses) {
+            https.get("https://api.yelp.com/v3/businesses/" + match.businesses[0].id, options, (resp) => {
+              let details = "";
+              resp.on("data", (data) => {
+                details += data;
+              });
+              resp.on("end", () => {
+                details = JSON.parse(details);
+                console.log(details);
+              });
+              resp.on("error", (err) => {
+                console.log("Error getting details for business: " + err.message);
+              })
+            });
+          } else if (match.error) {
+            console.log("Error matching business: " + match.error.description);
+          } else {
+            console.log("Unknown error matching business.");
+          }
+        });
+        resp.on("error", (err) => {
+          console.log("Error matching business: " + err.message);
+        });
+      });
+      */
+    });
   } else {
     res.writeHead(404, {"Content-Type": "text/html"});
     return res.end("404 Not Found");
